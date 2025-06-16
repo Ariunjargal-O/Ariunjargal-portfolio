1e354160ef9a3fb6b494cb9dff7bd2636e3c1fe1
Ariunjargal-O <ariunjargal.ochirpurev@gmail.com>
2025-06-16 16:07:02 +0800

    deploy-3

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useRef } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 import { toast } from "sonner";
 import { Mail, Send, CheckCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
